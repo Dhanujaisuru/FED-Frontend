1774150b03058e58ca7838c8272a428824791a95
Dhanuja Isuru <128521002+Dhanujaisuru@users.noreply.github.com>
2025-01-09 20:58:09 +0530

    week-11

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -2,19 +2,43 @@ import Hero from "./../Hero"
 import Navigation from "./../Navigation"
 import Products from "./../Products"
 import PaginationComponent from "./../Pagination";
+import { useState } from "react";
 
 function HomePage() {
-    const name = null
-    const cartCount = 2;
-  
-    return (
-      <div>
-        <Navigation name={name} cartCount={cartCount} />
-        <Hero />
-        <Products />
-        <PaginationComponent />
-      </div>
-    );
-  }
-  
-  export default HomePage;
+  const name = null;
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (product) => {
+    const foundItem = cart.find((item) => item.product._id === product._id);
+    if (foundItem) {
+      setCart(
+        cart.map((cartItem) =>
+          cartItem.product._id === product._id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+      );
+      return;
+    }
+    setCart([...cart, { product: product, quantity: 1 }]);
+  };
+
+  const getCartQuantity = () => {
+    let count = 0;
+    cart.forEach((item) => {
+      count += item.quantity;
+    });
+    return count;
+  };
+
+  return (
+    <div>
+      <Navigation name={name} cartCount={getCartQuantity()} />
+      <Hero />
+      <Products handleAddToCart={handleAddToCart} />
+      <PaginationComponent />
+    </div>
+  );
+}
+
+export default HomePage;
