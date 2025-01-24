cef36714a379f2a0d6fc2b737212fed7668895f9
Dhanuja Isuru <128521002+Dhanujaisuru@users.noreply.github.com>
2025-01-24 20:12:31 +0530

    week-14- code

--- src/pages/home.page.jsx
+++ src/pages/home.page.jsx
@@ -4,42 +4,51 @@ import Products from "./../Products"
 import PaginationComponent from "./../Pagination";
 import { useState } from "react";
 
-
 function HomePage() {
-  const name = null;
-  const [cart, setCart] = useState([]);
-
-  const handleAddToCart = (product) => {
-    const foundItem = cart.find((item) => item.product._id === product._id);
-    if (foundItem) {
-      setCart(
-        cart.map((cartItem) =>
-          cartItem.product._id === product._id
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        )
-      );
-      return;
-    }
-    setCart([...cart, { product: product, quantity: 1 }]);
-  };
-
-  const getCartQuantity = () => {
-    let count = 0;
-    cart.forEach((item) => {
-      count += item.quantity;
-    });
-    return count;
-  };
-
   return (
-    <div>
-      <Navigation name={name} cartCount={getCartQuantity()} />
+    <main>
       <Hero />
-      <Products handleAddToCart={handleAddToCart} />
+      <Products />
       <PaginationComponent />
-    </div>
+    </main>
   );
 }
+
+// function HomePage() {
+//   const name = null;
+//   const [cart, setCart] = useState([]);
+
+//   const handleAddToCart = (product) => {
+//     const foundItem = cart.find((item) => item.product._id === product._id);
+//     if (foundItem) {
+//       setCart(
+//         cart.map((cartItem) =>
+//           cartItem.product._id === product._id
+//             ? { ...cartItem, quantity: cartItem.quantity + 1 }
+//             : cartItem
+//         )
+//       );
+//       return;
+//     }
+//     setCart([...cart, { product: product, quantity: 1 }]);
+//   };
+
+//   const getCartQuantity = () => {
+//     let count = 0;
+//     cart.forEach((item) => {
+//       count += item.quantity;
+//     });
+//     return count;
+//   };
+
+//   return (
+//     <div>
+//       <Navigation name={name} cartCount={getCartQuantity()} />
+//       <Hero />
+//       <Products handleAddToCart={handleAddToCart} />
+//       <PaginationComponent />
+//     </div>
+//   );
+// }
 
 export default HomePage;
